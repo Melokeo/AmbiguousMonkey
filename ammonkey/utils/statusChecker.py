63d1af2af81d61e43c222b40eb5d7371580b2401
Melokeo <penkonno@gmail.com>
2025-07-21 14:11:41 -0400

    fixed typing problems

--- ammonkey/utils/statusChecker.py
+++ ammonkey/utils/statusChecker.py
@@ -3,20 +3,21 @@ Tracking status checker
 p.s. daet = date-animal-experiment-task
 '''
 from pathlib import Path
-from dataclasses import dataclass
-from typing import Optional, Union, List
+from dataclasses import dataclass, field
+from typing import Any
 import re
 from tqdm import tqdm
 from .. import monkeyUnityv1_8 as mky
 from .pullAniAll import getAllDates, convertRawToData, getCSVPathUnder
 from .silence import silence
+from ..core.daet import DAET
 
 @dataclass
 class checkpoint:
     name: str
-    subdir: Optional[list[str]] = None
-    condition: list[str] = None
-    interpret: Optional[dict[Union[tuple[bool],int], str]] = None
+    subdir: list[str] = field(default_factory=lambda: [''])
+    condition: list[str] = field(default_factory=list)
+    interpret: dict[Any, str] | None = None
 
     def __post_init__(self):
         if not self.subdir:
@@ -26,14 +27,15 @@ class checkpoint:
         if len(self.condition) == 0:
             raise ValueError('checkpoint must have a condition')
     
-    def check(self, data_path:Union[Path,str], daet:str) -> tuple[bool]:
+    def check(self, data_path: Path | str, daet: DAET) -> tuple[bool]:
         if isinstance(data_path, str): data_path = Path(data_path)
         subdir = subph(self.subdir, daet)
         cond = subph(self.condition, daet)
 
         workdir = data_path.joinpath(*subdir)
-        if not workdir.exists(): raise FileNotFoundError
-
+        if not workdir.exists(): # raise FileNotFoundError(workdir)
+            return (False,)
+        
         stat = []
         for p in cond:
             r = re.compile(p)
@@ -63,7 +65,7 @@ checkpoints = {
     'sync_L': checkpoint(
         'SyncL',
         subdir=['SynchronizedVideos', '`', 'L'],
-        condition=['`-cam1\.mp4', '`-cam2\.mp4'],
+        condition=[r'`-cam1\.mp4', r'`-cam2\.mp4'],
         interpret={
             2: 'OK',
             1: 'Missing 1 video',
@@ -73,7 +75,7 @@ checkpoints = {
     'sync_R': checkpoint(
         'SyncR',
         subdir=['SynchronizedVideos', '`', 'R'],
-        condition=['`-cam3\.mp4', '`-cam4\.mp4'],
+        condition=[r'`-cam3\.mp4', r'`-cam4\.mp4'],
         interpret={
             2: 'OK',
             1: 'Missing 1 video',
@@ -83,39 +85,39 @@ checkpoints = {
     'skipsync': checkpoint(
         '.skipSync',
         subdir=['SynchronizedVideos', '`'],
-        condition=['\.skipSync'],
+        condition=[r'\.skipSync'],
         interpret=skip_interp
     ),
     'skipdet': checkpoint(
         '.skipDet',
         subdir=['SynchronizedVideos', '`'],
-        condition=['\.skipDet'],
+        condition=[r'\.skipDet'],
         interpret=skip_interp
     ),
     'dlc_L': checkpoint(
         'DLC_L',
         subdir=['SynchronizedVideos', '`', 'L'],
-        condition=['`-cam1DLC.*?_filtered\.h5', '`-cam1DLC.*?\.h5',
-                '`-cam2DLC.*?_filtered\.h5', '`-cam2DLC.*?\.h5'],
+        condition=[r'`-cam1DLC.*?_filtered\.h5', r'`-cam1DLC.*?\.h5',
+                r'`-cam2DLC.*?_filtered\.h5', r'`-cam2DLC.*?\.h5'],
         interpret=dlc_interp
     ),
     'dlc_R': checkpoint(
         'DLC_R',
         subdir=['SynchronizedVideos', '`', 'R'],
-        condition=['`-cam3DLC.*?_filtered\.h5', '`-cam3DLC.*?\.h5',
-                '`-cam4DLC.*?_filtered\.h5', '`-cam4DLC.*?\.h5'],
+        condition=[r'`-cam3DLC.*?_filtered\.h5', r'`-cam3DLC.*?\.h5',
+                r'`-cam4DLC.*?_filtered\.h5', r'`-cam4DLC.*?\.h5'],
         interpret=dlc_interp
     ),
     'skipdlc_L': checkpoint(
         'skipdlc_L',
         subdir=['SynchronizedVideos', '`', 'L'],
-        condition=['\.skipDLC'],
+        condition=[r'\.skipDLC'],
         interpret=skip_interp
     ),
     'skipdlc_R': checkpoint(
         'skipdlc_L',
         subdir=['SynchronizedVideos', '`', 'R'],
-        condition=['\.skipDLC'],
+        condition=[r'\.skipDLC'],
         interpret=skip_interp
     ),
 }
@@ -125,14 +127,14 @@ anipose_interp = {
     3: "x Only 3 *.h5 found",
     2: "x Only 2 *.h5 found",
     1: "x Only 1 *.h5 found",
-    0: "x Only 3 *.h5 found",
+    0: "No *.h5 found",
 }
 
 checkpoints_anipose = {
     'pose-2d-f': checkpoint(
         'pose-2d-filtered',
         subdir=['anipose', '`', 'pose-2d-filtered'],
-        condition=['`-cam1\.h5', '`-cam2\.h5', '`-cam3\.h5', '`-cam4\.h5'],
+        condition=[r'`-cam1\.h5', r'`-cam2\.h5', r'`-cam3\.h5', r'`-cam4\.h5'],
         interpret={
             4: "OK",
             3: "x Only 3 *.h5 found",
@@ -144,7 +146,7 @@ checkpoints_anipose = {
     'pose-3d': checkpoint(
         'pose-3d',
         subdir=['anipose', '`', 'pose-3d'],
-        condition=['`\.csv'],
+        condition=[r'`\.csv'],
         interpret={
             1: "OK",
             0: "x"
@@ -153,7 +155,7 @@ checkpoints_anipose = {
     'clean': checkpoint(
         'clean',
         subdir=['clean'],
-        condition=['`\.csv'],
+        condition=[r'`\.csv'],
         interpret={
             1: "OK",
             0: "x"
@@ -165,7 +167,7 @@ checkpoints_extra = {
     'csv_aw': checkpoint(
         'Anywhere CSV',
         subdir=[''],
-        condition=['`\.csv'],
+        condition=[r'`\.csv'],
         interpret={
             1: "Found CSV elsewhere",
             0: 'FNF'
@@ -175,8 +177,9 @@ checkpoints_extra = {
 
 chk_dict = checkpoints | checkpoints_anipose | checkpoints_extra
 
-def subph(x: Union[str, List[str]], replacement: str) -> Union[str, List[str]]:
+def subph(x: str | list[str], replacement: str | DAET) -> str | list[str]:
     '''SUBstitute PlaceHolder (`)'''
+    replacement = str(replacement)
     if isinstance(x, str):
         return x.replace('`', replacement)
     elif isinstance(x, list):
@@ -184,16 +187,20 @@ def subph(x: Union[str, List[str]], replacement: str) -> Union[str, List[str]]:
     return x
 
 def checkDaetValidity() -> bool:
-    pass
+    return True
 
-def checkOnDate(praw:Path, pdat:Path=None) -> None:
+def checkOnDate(praw:Path|str, pdat:Path|str|None=None) -> None:
+    praw = Path(praw)
     if pdat is None:
-        pdat = Path(str(praw).replace('DATA_RAW', 'DATA'))
+        pdat = str(praw).replace('DATA_RAW', 'DATA')
+    pdat = Path(pdat)
     print(f'\n===== {pdat.name} =====')
-    daets = mky.getTasks(PPATH_RAW=str(praw), task=mky.Task.All)
+
+    daets = mky.getTasksInDAET(PPATH_RAW=str(praw), task=mky.Task.All)
     if daets:
-        for daet in daets:
-            if 'calib' in daet.lower(): continue
+        for d in daets:
+            daet = DAET.fromString(d)   #FIXME vulnerable
+            if daet.isCalib: continue
             print(f'--- {daet} ---')
             if Path(pdat/'clean').exists:
                 if sum(chk_dict['clean'].check(pdat, daet)) == 1:

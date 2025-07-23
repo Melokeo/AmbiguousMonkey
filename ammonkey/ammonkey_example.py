4b2a2e8e743c2e81648bd2f30b707430024774ba
Melokeo <penkonno@gmail.com>
2025-07-23 16:20:30 -0400

    updated example usage and annotations

--- ammonkey/ammonkey_example.py
+++ ammonkey/ammonkey_example.py
@@ -1,27 +1,26 @@
 '''
-Usage example for ammonkey
+Usage example for ammonkey, w/ annoying annotations
 '''
+# for actual usage one can simply do wildcat import actually.
+# ===============================
+# 1. data setup & create note obj
+from ammonkey import Path, dataSetup, ExpNote, DAET, Task
+# raw data folder to process. remember to use raw string r''
+raw_path = Path(r'P:\projects\monkeys\Chronic_VLL\DATA_RAW\Pici\2025\06\20250620')
 
-from ammonkey import (
-    dataSetup, ExpNote, VidSynchronizer,
-    initDlc, DLCProcessor, DLCModel, modelPreset,
-    createProcessor_Pull,
-    AniposeProcessor, violentCollect,
-    CamGroup
-)
-
-# raw data folder to process, remember to use raw string r''
-raw_path = r'P:\projects\monkeys\Chronic_VLL\DATA_RAW\Pici\2025\06\20250610'
-
-# create data folder
+# create data folder & shortcuts
 dataSetup(raw_path=raw_path)
 
 # create note object
 note = ExpNote(raw_path)
 
 # print entries if you want
-print(note.data_path)
-print('\n'.join(note.daets))
+print(f'{note.data_path=}')
+print('\n\t'.join(str(note.daets)))
+
+# ===============================
+# 2.1 setup vid sync
+from ammonkey import VidSynchronizer, CamConfig, CamGroup
 
 # create synchronizer
 synchronizer = VidSynchronizer(note)
@@ -29,11 +28,14 @@ synchronizer = VidSynchronizer(note)
 # set rois for LED detection
 synchronizer.setROI()
 
-# examples for configuring camera setups 
-synchronizer.cam_config.led_colors[2] = 'Y'
-synchronizer.cam_config.groups[3] = CamGroup.RIGHT
+# 2.2 examples for configuring camera setups 
+# by default just check if cam2 LED is green or yellow; others no need to configure.
+# camera indexing starts from 1, not 0
+synchronizer.cam_config.led_colors[1] = 'Y'         # set cam1 detection to anticipate Yellow LED
+synchronizer.cam_config.led_colors[2] = 'G'         # set cam2 detection to anticipate Green LED
+synchronizer.cam_config.groups[3] = CamGroup.RIGHT  # set cam3 to belong to CamGroup.RIGHT (same as default)
 
-# detect then synchronize all daets
+# 2.3 detect then synchronize all daets
 results = synchronizer.syncAll()
 print(results)
 
@@ -43,22 +45,27 @@ print(results)
 # recommend to check the audio plot to ensure correct sync.
 # ================================================================
 
-# model set dictionary, used to determine which model to use on each cam group folders
-model_dict = {
-    CamGroup.LEFT:  modelPreset('Pull-L'),
-    CamGroup.RIGHT: modelPreset('Pull-R'),
-}
+# ===============================
+# 3. create DLC processor and run it.
+from ammonkey import (
+    DLCProcessor, DLCModel, initDlc,
+    modelPreset, createProcessor_BBT, createProcessor_Brkm,
+    createProcessor_Pull, createProcessor_TS,
+)
 
-# create dlc processor object
-dp = DLCProcessor(note, model_dict=model_dict)
-
-# or, just use preset defined in the package
+# 3.1 for example we want to analyze pull-arm (4-cam setup)
 dp = createProcessor_Pull(note)
 
 # import deeplabcut. it's slow
 initDlc()
 
 # run deeplabcut analysis. takes a century.
+dlc_results = dp.batchProcess()
+print(dlc_results)
+
+# 3.2 now try to run BBT (2-cam setup)
+dp = createProcessor_BBT(note)
+initDlc()
 dlc_results = dp.batchProcess()
 print(dlc_results)
 
@@ -69,18 +76,25 @@ print(dlc_results)
 # the 'separate' folder stores single model outputs
 # ================================================================
 
-# create anipose processor object
-ap = AniposeProcessor(note)
+# ===============================
+# 4. anipose setup and run
+from ammonkey import runAnipose, AniposeProcessor, getUnprocessedDlcData
 
-# check which config will be used, etc.
+# quickly run anipose on a folder
+runAnipose(note, model_set_name='Brkm-20250620_5608')
+
+# or manually, create anipose processor object
+ap = AniposeProcessor(note, model_set_name='Brkm-20250620_5608')
+
+# check which config and calib will be used, etc.
 print(ap.info)
 
 # anipose calibration
 ap.setupCalibs()
-ap.calibrate()
+ap.calibrateCLI()
 
 # triangulate. another century passed by.
-ap.triangulate()
+ap.triangulateCLI()
 
 # ================================================================
 # now you should see under anipose/ there is a folder named after the model set
@@ -88,9 +102,58 @@ ap.triangulate()
 # ================================================================
 
 # collect csvs
+from ammonkey import violentCollect
 violentCollect(
     ani_path=ap.ani_root_path, 
     clean_path=(note.data_path / 'clean')
 )
 
-# now the csvs should be collected in data_path/clean. HAPPY???
+# now the csvs should be collected in data_path/clean. HAPPY???!
+
+
+
+
+
+# ===========
+# Supplements
+# ===========
+
+# playing with ExpNote
+p = Path(r'P:\projects\monkeys\Chronic_VLL\DATA_RAW\Pici\2025\06\20250620')
+note = ExpNote(p)
+
+print(note.getAllTaskTypes())
+# [<Task.BBT: 3>, <Task.CALIB: 6>, <Task.BRKM: 4>]
+
+print(note.checkSanity())
+# True
+
+print(note.daets)
+# [DAET('20250620-Pici-Calibration-0.0'), DAET('20250620-Pici-Brinkman-1.0'), DAET('20250620-Pici-Brinkman-2.0')...
+print(note.getSummary())
+# {'total_entries': 15, 'valid_entries': 15, 'void_entries': 0, 'calibration_entries': 1, 'processable_entries': 15}
+
+# keep only calib and bbt
+note_filtered = note.applyTaskFilter([Task.CALIB, Task.BBT])
+# equivalent as excluding brkm.
+note_filtered = note.applyTaskFilter([Task.BRKM], exclude=True)
+
+print(note_filtered.getAllTaskTypes())
+# [<Task.BBT: 3>, <Task.CALIB: 6>]
+
+calib = note_filtered.daets[0]
+bbt_8 = note_filtered.daets[1]
+print(f'{calib=}, {bbt_8=}')
+# calib=DAET('20250620-Pici-Calibration-0.0'), bbt_8=DAET('20250620-Pici-BBT-8.0')
+print(f'{calib.isCalib=}, {bbt_8.isCalib=}')
+# calib.isCalib=True, bbt_8.isCalib=False
+
+note_filtered.getVidSetIdx(daet=calib)
+# [1024, 971, 1184, 1146]
+note_filtered.checkVideoExistence(no=1) # same as passing daet=bbt_8, cuz its index is 1
+# {0: True, 1: True, 2: True, 3: True}
+
+note_filtered.getDaetSyncRoot(daet=bbt_8)
+# WindowsPath('P:/projects/monkeys/Chronic_VLL/DATA/Pici/2025/06/20250620/SynchronizedVideos/20250620-Pici-BBT-8.0')
+note_filtered.getDaetDlcRoot(daet=bbt_8)
+# WindowsPath('P:/projects/monkeys/Chronic_VLL/DATA/Pici/2025/06/20250620/SynchronizedVideos/20250620-Pici-BBT-8.0/DLC')

55c8fafbba5e0a543aad5d8781daa2dea3c4b442
Melokeo <penkonno@gmail.com>
2025-10-30 11:42:07 -0400

    update init

--- ammonkey/__init__.py
+++ ammonkey/__init__.py
@@ -8,22 +8,23 @@
 from pathlib import Path
 
 from .core.daet import DAET
-from .core.expNote import ExpNote, Task, iter_notes
-from .core.camConfig import CamGroup, CamConfig
+from .core.expNote import ExpNote, Task, iter_notes, iter_xlsx, get_xlsx_dates
+from .core.camConfig import CamGroup, CamConfig, Camera
 
 from .core.sync import syncVideos, VidSynchronizer, SyncConfig, SyncResult
 
 from .core.dlc import DLCProcessor, DLCModel
 from .core.dlc import (
-    createProcessor_BBT, createProcessor_Brkm, 
-    createProcessor_Pull, createProcessor_TS,
-    modelPreset, initDlc
+    modelPreset, initDlc, model_factory, available_models, available_dp,
 )
 from .core.dlcCollector import mergeDlcOutput, getUnprocessedDlcData
 from .core.ani import AniposeProcessor, runAnipose
 from .core.finalize import violentCollect
 from .core.fileOp import dataSetup
 
-from .utils.ol_logging import ColorLoggingFormatter
+from .utils.ol_logging import ColorLoggingFormatter, set_colored_logger
 
 import logging
+
+lg = set_colored_logger(__name__)
+lg.info('test')

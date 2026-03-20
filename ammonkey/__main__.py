b8dabe003812ad87c8b8ba9f4cacec28e2c453eb
Melokeo <penkonno@gmail.com>
2026-03-20 15:10:38 -0400

    change cli entry point

--- ammonkey/__main__.py
+++ ammonkey/__main__.py
@@ -1,9 +1,22 @@
-import flet as ft
-from .gui_v3.flet_main import AmmApp
+import argparse
+import sys
 
 def main() -> None:
-    '''main gui entry, also used in pyproject'''
+    '''main entry, also used in pyproject'''
+    parser = argparse.ArgumentParser(prog="ammonkey")
+    parser.add_argument("-u", "--util", type=str, help="Run a utility by name")
+    args, remaining = parser.parse_known_args()
+
+    if args.util:
+        # route -u to utils
+        sys.argv = ["ammonkey.utils", "-u", args.util] + remaining
+        from ammonkey.utils.__main__ import main as utils_main
+        utils_main()
+        return
+
+    import flet as ft
+    from .gui_v3.flet_main import AmmApp
     ft.app(AmmApp())
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
